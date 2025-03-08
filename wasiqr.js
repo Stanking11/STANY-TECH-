492faa516658a0f37aee8a98e59e8d8909d4ecce
B.M.B-TECH1 <hanchobmb@gmail.com>
2025-03-08 23:27:19 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -60,24 +60,21 @@ router.get('/', async (req, res) => {
 	
 				   let WASI_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃B.M.B-XMD SESSION IS 
+┃STANY-TECH-XMD SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘 BMB XMD 𖥘
+❶ || Creator = 𖥘 STANY XMD 𖥘
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z
+❷ || WhattsApp Channel = https://whatsapp.com/channel/0029Vb8ixZ6DOQIPoX014p1k
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 Please Follow My Support Channel
-Wanna talk to me?👉 https://whatsapp.com/channel/0029VaoadqE84OmC8xlVsQ1M 👈
+Wanna talk to me?👉 https://github.com/Stanking11/STANY-TECH-T-XMD 👈
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-deploy bot 👉https://github.com/bmb200/B.M.B-XMD👈
+contact me1 👉 https://wa.link/8v4huy 👈
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-contact me1 👉255772341432
-contact me2 👉255741752020
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2025-2050 𝙱.𝙼.𝙱-𝚇𝙼𝙳*
+©*2025-2050 stany-𝚇𝙼𝙳*
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
